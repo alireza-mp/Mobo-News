5bd997d849fd04048270dcbb01a25c3793bb940f
alireza_mo <alirezamomenpour01@gmail.com>
2022-09-14 11:56:25 +0430

    change status bar color

--- app/src/main/java/com/example/mobonews/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mobonews/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mobonews.presentation
 
+import android.app.Activity
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -7,9 +8,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.mobonews.presentation.navigation.baseNavigation.MoboNewsNavigation
 import com.example.mobonews.presentation.theme.MoboNewsTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
@@ -18,15 +21,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val locale = Locale("fa")
-        Locale.setDefault(locale)
-        val config: Configuration = resources.configuration
-        config.setLocale(locale)
-        resources.updateConfiguration(config, resources.displayMetrics)
+        setAppLanguage(this)
 
         setContent {
-
             MoboNewsTheme {
+                SetStatusBarColor()
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
@@ -38,3 +37,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+private fun setAppLanguage(activity: Activity) {
+    val locale = Locale("fa")
+    Locale.setDefault(locale)
+    val config: Configuration = activity.resources.configuration
+    config.setLocale(locale)
+    activity.resources.updateConfiguration(config, activity.resources.displayMetrics)
+}
+
+@Composable
+private fun SetStatusBarColor() {
+    val uiController = rememberSystemUiController()
+    uiController.setStatusBarColor(color = MaterialTheme.colors.background)
+}
